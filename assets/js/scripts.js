0211ce612da5504b3049b5f4f1382427cc1ac854
Anne034 <annearaujo034@gmail.com>
2024-11-20 19:28:04 -0300

    first

---
+++ assets/js/scripts.js
@@ -0,0 +1,57 @@
+const toggleTheme = document.getElementById("toggleTheme");
+const rootHtml = document.documentElement;
+const accordionHeaders = document.querySelectorAll(".accordion__header");
+const menuLinks = document.querySelectorAll(".menu__link");
+
+function changeTheme() {
+  const currentTheme = rootHtml.getAttribute("data-theme");
+  const newTheme = currentTheme === "dark" ? "light" : "dark";
+
+  rootHtml.setAttribute("data-theme", newTheme);
+  localStorage.setItem("theme", newTheme);
+
+
+  toggleTheme.classList.toggle("bi-sun");
+  toggleTheme.classList.toggle("bi-moon-stars");
+}
+
+
+function loadSavedTheme() {
+  const savedTheme = localStorage.getItem("theme");
+
+  if (savedTheme) {
+    rootHtml.setAttribute("data-theme", savedTheme);
+
+
+    if (savedTheme === "light") {
+      toggleTheme.classList.add("bi-sun");
+      toggleTheme.classList.remove("bi-moon-stars");
+    } else {
+      toggleTheme.classList.add("bi-moon-stars");
+      toggleTheme.classList.remove("bi-sun");
+    }
+  }
+}
+
+
+loadSavedTheme();
+
+
+toggleTheme.addEventListener("click", changeTheme);
+
+
+accordionHeaders.forEach(header => {
+  header.addEventListener("click", () => {
+    const accordionItem = header.parentElement;
+    const accordionActive = accordionItem.classList.contains("active");
+
+    accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active");
+  });
+});
+
+menuLinks.forEach(item => {
+  item.addEventListener("click", () => {
+    menuLinks.forEach(i => i.classList.remove("active"));
+    item.classList.add("active");
+  });
+});
